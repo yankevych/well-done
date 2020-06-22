b68409aaed8c904ee79d5c5a1fd9ed6e28d32e26
yankevych <37641242+yankevych@users.noreply.github.com>
2020-06-23 01:53:34 +0300

    Update settings.py

--- tenya_pro/tenya_pro/settings.py
+++ tenya_pro/tenya_pro/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'e!!r$&6qdfwer$)^60w=0qu4ja)fgzci6c!qxefmr0c7wxp4(('
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
